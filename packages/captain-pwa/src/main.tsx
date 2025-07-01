4a60d1da7dc3ed5522b35a3f97111ad10169ddc1
Chris Rodriguez <melheffe@gmail.com>
2025-06-30 20:43:36 -0400

    Fix: Remove ALL CSS imports from captain PWA

--- packages/captain-pwa/src/main.tsx
+++ packages/captain-pwa/src/main.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
-import '@mantine/core/styles.css';
-import '@mantine/notifications/styles.css';
-import './index.css';
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
